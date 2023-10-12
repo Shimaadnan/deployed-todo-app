64f3855c5a86c3a863acc469a51616fb55f8dc32
Shima <shimaadnanbasmenj@gmail.com>
2023-10-12 10:54:31 +0100

    Delete query on server

--- server/server.js
+++ server/server.js
@@ -59,4 +59,15 @@ app.put('/todos/:id',async(req,res)=>{
         }
         catch(err){console.error(err);}
 })
+app.delete('todos/:id',async(req,res)=>{
+    const {id}=req.params
+    try{
+        const deleteTodo=await pool.query('DELETE FROM todos WHERE id=$1',[id])
+        res.json(deleteTodo)
+    }
+    catch(err){
+        console.error(err);
+    }
+})
+
 app.listen(PORT,()=>console.log(`Server is running on PORT ${PORT}`))
