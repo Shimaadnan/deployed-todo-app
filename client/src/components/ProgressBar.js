41696fcbbb0e6171e240a65fb7cb16d4b47281bc
Shima <shimaadnanbasmenj@gmail.com>
2023-10-28 05:40:43 +0100

    ProgressBar

--- client/src/components/ProgressBar.js
+++ client/src/components/ProgressBar.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
 export default function ProgressBar({progress}) {
+  const colors=[
+    'rgb(255,241,161)',
+    'rgb(255,175,163)',
+    
+    'rgb(108,115,148) ',
+    'rgb(141,181,145)'
+  ]
+  const randomcolors=colors[Math.floor(Math.random()*colors.length)]
   return (
     <div className='outer-bar'>
       <div className='inner-bar'
-      style={{width:`${progress}%` ,backgroundColor:'red'}}>
+      style={{width:`${progress}%` ,backgroundColor:randomcolors}}>
         
       </div>
     </div>
