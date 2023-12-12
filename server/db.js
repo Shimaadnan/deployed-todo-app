492ca55a6bf3a60446b74d79eda6b4983f96bcd9
Shima <shimaadnanbasmenj@gmail.com>
2023-12-12 11:01:20 +0000

    Confirming the local connection

--- server/db.js
+++ server/db.js
@@ -2,14 +2,14 @@ const Pool=require('pg').Pool
 require('dotenv').config()
 
 const pool=new Pool({
-    // user:process.env.USERNAME,
-    // password:process.env.PASSWORD,
-    // host:process.env.HOST,
-    // port:process.env.DBPORT,
-    // database:'homework'
+    user:process.env.USERNAME,
+    password:process.env.PASSWORD,
+    host:process.env.HOST,
+    port:process.env.DBPORT,
+    database:'homework'
 
 
-    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+    // connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 
 
 })
