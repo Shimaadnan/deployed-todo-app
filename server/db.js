efdfc884ee1622af9458a2ccaa9171129e5cf05e
Shima <shimaadnanbasmenj@gmail.com>
2024-01-12 17:15:26 +0000

    fix db connection

--- server/db.js
+++ server/db.js
@@ -16,6 +16,16 @@ require('dotenv').config()
 const pool = new Pool({
     connectionString: process.env.POSTGRES_URL + "?sslmode=require",
   })
+  pool.connect(
+    (err)=>{
+        if(err){
+            throw err
+        }
+        else{
+            console.log('connected correctly');
+        }
+    }
+  )
  
 
 module.exports=pool
